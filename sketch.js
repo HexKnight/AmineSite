99c3cbb386384766a3d66c18956ac80f70f66f3b
Amine Tahiri Alaoui <alaouiamine23@gmail.com>
2017-08-07 11:14:30 +0100

    Draw rain

--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@ function setup(){
   background(53);
   var rain = [];
   for(int i=0; i<300; i++)
-    rain[i] = new drop(random(width), -600, 20, 50, 0, -40);
+    rain[i] = new drop(random(width), 0, 20, 50, 0, -40);
 }
 
 function draw(){
-  fill(200, 0, 0);
-  ellipse(width/2, height/2, 100, 100);
-  
+  fill(255);
+  strokeWieght(3);
+  for(int i=0; i<300; i++)
+    rain[i].draw;
 }
 
 var drop = function(x, y, width, height, vilocityX, vilocityY){
@@ -19,4 +20,10 @@ var drop = function(x, y, width, height, vilocityX, vilocityY){
   this.height = height;
   this.vilocityX = vilocityX;
   this.vilocityY = vilocityY;
+  draw : function(){
+    this.x += this.vilocityX;
+    this.y += this.vilocityY;
+    if(this.y<-height)this.y = 0;
+    rect(this.x, this.y, this.width, this.height);
+  }
 }
