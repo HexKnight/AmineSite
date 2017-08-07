8192e69623d1969eda6d0d4d4fdad47cc98033e1
Amine Tahiri Alaoui <alaouiamine23@gmail.com>
2017-08-07 19:00:25 +0100

    Dump

--- sketch.js
+++ sketch.js
@@ -19,12 +19,12 @@ var rain = [];
 function setup(){
   createCanvas(1280, 720);
   background(53);
-  for(int i=0; i<300; i++)
+  for(var i=0; i<300; i++)
     rain[i] = new drop(random(1280), 0, 20, 50, 0, -40);
 }
 
 function draw(){
   fill(255);
-  for(int i=0; i<300; i++)
+  for(var i=0; i<300; i++)
     rain[i].draw();
 }
