afaa66485e0b774c8f0456a01a143d204396573b
Amine Tahiri Alaoui <alaouiamine23@gmail.com>
2017-08-07 11:19:46 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,14 +3,14 @@ function setup(){
   background(53);
   var rain = [];
   for(int i=0; i<300; i++)
-    rain[i] = new drop(random(width), 0, 20, 50, 0, -40);
+    rain[i] = new drop(random(1280), 0, 20, 50, 0, -40);
 }
 
 function draw(){
   fill(255);
   strokeWieght(3);
   for(int i=0; i<300; i++)
-    rain[i].draw;
+    rain[i].draw();
 }
 
 var drop = function(x, y, width, height, vilocityX, vilocityY){
@@ -23,7 +23,7 @@ var drop = function(x, y, width, height, vilocityX, vilocityY){
   this.draw = function(){
     x += vilocityX;
     y += vilocityY;
-    if(y<-1280)this.y = 0;
+    if(y<-720)this.y = 0;
     rect(x, y, width, height);
   }
 }
